35ffbba7cd8923e0306011bf241c9deff7f39101
lucaslima94 <lcdsdlima@inf.ufpel.edu.br>
2016-04-29 22:24:47 +0100

    cliente servidor usando autenticação basica

--- cliente.py
+++ cliente.py
@@ -1,17 +1,31 @@
+import json
 import socket
 HOST = socket.gethostbyname(socket.gethostname())     # Endereco IP do Servidor
-PORT = 50000         # Porta que o Servidor esta
+PORT = 30000         # Porta que o Servidor esta
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 dest = (HOST, PORT)
 tcp.connect(dest)
 resposta=tcp.recv(1024)
 if resposta=="ack":
-	print resposta
-	print 'Para sair use CTRL+X\n'
-	msg = raw_input()
-	while msg <> '\x18':
-		tcp.send (msg)
-		print "numero de mensagens enviadas:",tcp.recv(1024)
+	print "nome usuario"
+	user=raw_input()
+	print "senha"
+	password=raw_input()
+	line=user+' '+password
+	tcp.send(line)
+	resposta=tcp.recv(1024)
+	if(resposta=="500"):
+		print "conexao iniciada"
+		print 'Para sair use CTRL+X\n'
 		msg = raw_input()
-	tcp.close()
+		while msg <> '\x18':
+			tcp.send (msg)
+			print "numero de mensagens enviadas:",tcp.recv(1024)
+			msg = raw_input()
+		tcp.close()
+	if(resposta=="501"):
+		print "usuario ou senha incorretos"
+		tcp.close()
+		exit()
+		
 
--- servidor.py
+++ servidor.py
@@ -1,21 +1,34 @@
+import json
 import socket
 HOST = ''              # Endereco IP do Servidor
-PORT = 50000            # Porta que o Servidor esta
+PORT = 30000            # Porta que o Servidor esta
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 orig = (HOST, PORT)
 tcp.bind(orig)
 tcp.listen(1)
 contador=0
+user="lucas"
+password="1234"
 while True:
     con, cliente = tcp.accept()
     con.send("ack")
-    print 'Concetado por', cliente
-    while True:
-        msg = con.recv(1024)
-        if not msg: break
-        print cliente, msg
-        contador=contador+1
-        con.send(str(contador))
-    print 'Finalizando conexao do cliente', cliente
-    con.close()
-    exit()
+    msg=con.recv(1024)
+    users=msg.split(" ")
+    if(users[0]==user and users[1]==password):
+		con.send("500")
+		print 'Concetado por', cliente
+		while True:
+			msg = con.recv(1024)
+			if not msg: break
+			print cliente, msg
+			contador=contador+1
+			con.send(str(contador))
+		print 'Finalizando conexao do cliente', cliente
+		con.close()
+		exit()
+    if(users[0]!=user or users[1]!=password):
+		con.send("501")
+		tcp.close()
+		exit()
+		
+		
